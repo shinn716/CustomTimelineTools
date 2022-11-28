1f6e02de496417f0b00436b4c79f6816bdd6fdd1
DELTA\JOHN.TC.TSAI <john.tc.tsai@deltaww.com>
2022-11-28 17:20:35 +0800

    Support AVPro-media player.

---
+++ Assets/Packages/CustomTimelineTools/AVProCustomMediaPlayer~/CustomAvproMediaClip.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using RenderHeads.Media.AVProVideo;
+
+namespace Shinn.Timelinie
+{
+    public class CustomAvproMediaClip : PlayableBehaviour
+    {
+        public MediaPlayer mediaPlayer { get; set; }
+        public MediaPlayer.FileLocation fileLocation { get; set; }
+        public string mediaUrl { get; set; }
+        public bool loop { get; set; } = false;
+
+        private bool startOnce = false;
+
+        public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+        {
+            if (mediaPlayer != null)
+            {
+                if (startOnce)
+                    return;
+                startOnce = true;
+
+                mediaPlayer.OpenVideoFromFile(fileLocation, mediaUrl, loop);
+                mediaPlayer.Play();
+            }
+        }
+
+        // On clip end
+        public override void OnBehaviourPause(Playable playable, FrameData info)
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            var duration = playable.GetDuration();
+            var time = playable.GetTime();
+            var count = time + info.deltaTime;
+
+            if ((info.effectivePlayState == PlayState.Paused && count > duration) || Mathf.Approximately((float)time, (float)duration))
+            {
+                mediaPlayer.Stop();
+            }
+        }
+    }
+}
---
+++ Assets/Packages/CustomTimelineTools/AVProCustomMediaPlayer~/CustomAvproMediaPlayable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+using RenderHeads.Media.AVProVideo;
+
+namespace Shinn.Timelinie
+{
+    public class CustomAvproMediaPlayable : PlayableAsset
+    {
+        public ExposedReference<MediaPlayer> mediaPlayer;
+        public MediaPlayer.FileLocation fileLocation;
+        public string mediaUrl;
+        public bool loop = false;
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            var playable = ScriptPlayable<CustomAvproMediaClip>.Create(graph);
+            var selectClip = playable.GetBehaviour();
+
+            selectClip.mediaPlayer = mediaPlayer.Resolve(graph.GetResolver());
+            selectClip.mediaUrl = mediaUrl;
+            selectClip.fileLocation = fileLocation;
+            selectClip.loop = loop;
+            return playable;
+        }
+    }
+}
